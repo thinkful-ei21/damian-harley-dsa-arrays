895517c0028849a269881047833e94bf0fdc2efe
Damian Marius <damian.mm@live.com>
2018-07-25 12:02:45 -0700

    finished filtering array

--- arrayProblems.js
+++ arrayProblems.js
@@ -13,3 +13,15 @@ function spaceFiller(str) {
 }
 
 console.log(spaceFiller('tauhida parveen'));
+
+
+function lessThanFive(arr) {
+ let newArr = [];
+ for (let i = 0; i < arr.length; i++){
+    if (arr[i] >= 5) {
+      newArr.push(arr[i]);
+    }
+  }  
+  return newArr;
+}
+console.log(lessThanFive([3, 4, 5, 10, 15]));

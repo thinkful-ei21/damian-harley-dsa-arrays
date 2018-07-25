22670b49545994faadda23efe9faff54f2320ae2
harleyjj <harleyyesm@gmail.com>
2018-07-25 12:23:13 -0700

    merge sorted arrays

--- arrayProblems.js
+++ arrayProblems.js
@@ -16,8 +16,8 @@ console.log(spaceFiller('tauhida parveen'));
 
 
 function lessThanFive(arr) {
- let newArr = [];
- for (let i = 0; i < arr.length; i++){
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++){
     if (arr[i] >= 5) {
       newArr.push(arr[i]);
     }
@@ -25,3 +25,26 @@ function lessThanFive(arr) {
   return newArr;
 }
 console.log(lessThanFive([3, 4, 5, 10, 15]));
+
+//function maxContinuousSum
+
+function mergeArrays(arr1, arr2) {
+  let index2 = 0;
+  let newArr = [];
+  for (let i = 0; i < arr1.length; i++){
+    if (index2 < arr2.length) {
+      while(arr2[index2] < arr1[i]){
+        newArr.push(arr2[index2]);
+        index2++;
+        if(index2 >= arr2.length){
+          break;
+        }
+      }
+    }
+    newArr.push(arr1[i]);
+  }
+  return newArr;
+}
+
+console.log(mergeArrays([1, 3, 6, 8, 11],[2, 3, 5, 8, 9, 10]));
+
